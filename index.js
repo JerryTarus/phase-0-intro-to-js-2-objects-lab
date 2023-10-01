b8b25b7a555ec0737f33fb645c116f72ff10b75e
Jerry Tarus <jerrytarus@gmail.com>
2023-10-01 14:40:43 +0300

    Objects lab done

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+
+let employee = {
+    name: "Jerry Tarus",
+    streetAddress: "12th Main Belle Avenue",
+  };
+  
+  function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return { ...employee, [key]: value };
+  }
+  
+  function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+  }
+  
+  function deleteFromEmployeeByKey(employee, key, value) {
+    const newEmployee = { ...employee };
+    newEmployee[key] = value;
+  
+    return newEmployee;
+  }
+  
+  function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+  }
+  
